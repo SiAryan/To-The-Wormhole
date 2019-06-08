e0ba8b6f4510c19ba9343f7f5032a13471b4099c
OshoQ <aryan.joy@outlook.com>
2019-06-07 18:33:34 -0600

    Background added

--- Game/game.py
+++ Game/game.py
@@ -3,15 +3,15 @@ import objects
 pygame.init()
 
 
-screen_width = 1400
-screen_height = 700
+screen_width = 1500
+screen_height = 800
 screen = pygame.display.set_mode([screen_width,screen_height])
 pygame.display.set_caption("Asteroid belt")
 
 x = 50
-y = 50
-width = 20
-height = 30
+y = 500
+width = 64
+height = 64
 vel = 15
 
 screen_color = (255,255,255)
@@ -27,15 +27,25 @@ run = True
 
 walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
 walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
-bg = pygame.image.load('bg.jpg')
+bg = pygame.image.load('bg.png')
 char = pygame.image.load('standing.png')
 
+
+def redrawGramewindow():
+    #global walkCount
+    #screen.blit(bg,(0,0))
+
+    screen.blit(bg, (-200,-275))
+    #screen.fill([0, 0, 0])
+    pygame.draw.rect(screen, (255, 0, 0), (x, y, width, height))
+    pygame.display.update()
 
 
 
 
 while run:
-    pygame.time.delay(30)
+    redrawGramewindow()
+    pygame.time.delay(0)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
@@ -48,10 +58,10 @@ while run:
 
 
     if not(isJump):
-        #if keys[pygame.K_UP] and y > vel:
-        #    y -= vel
-        #if keys[pygame.K_DOWN] and y < screen_height - vel - height:
-        #   y += vel
+        if keys[pygame.K_UP] and y > vel:
+            y -= vel
+        if keys[pygame.K_DOWN] and y < screen_height - vel - height:
+           y += vel
         if keys[pygame.K_SPACE]:
             isJump = True
 
@@ -69,9 +79,7 @@ while run:
 
 
 
-    screen.fill([0,0,0])
-    pygame.draw.rect(screen, (255, 0, 0), (x, y, width, height))
-    pygame.display.update()
+
 
 pygame.quit()
 
